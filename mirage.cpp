f7b713cccd7f7f366f705ec5b2936150d44b3312
crepps <kevinx.crepps@intel.com>
2024-05-16 23:54:43 -0700

    Created Mirage class.

---
+++ mirage.cpp
@@ -0,0 +1,45 @@
+#include "mirage.h"
+
+Mirage::Mirage()
+{
+
+}
+int Mirage::ZipFile()
+{
+    // Create archive and open
+    int errCode = 0;
+    zip* archive = zip_open("archive.zip", ZIP_CREATE, &errCode);
+
+    // Open file, get size
+    std::ifstream file(filePath, std::ios::binary);
+    file.seekg(0, std::ios::end);
+    int n = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    // Store file data in buffer, close file
+    char* data = new char[n];
+    file.read(data, n);
+    file.close();
+
+    // Get filename from path
+    std::string fileName(filePath);
+    size_t strPos = fileName.find_last_of("\\");
+    fileName.erase(0, strPos + 1);
+
+    // Zip file using buffer, close archive
+    zip_source_t* source;
+    source = zip_source_buffer(archive, data, n, 0);
+    delete data;
+    zip_file_add(archive, fileName.c_str(), source, 0);
+
+    if (password != "\0")
+        zip_file_set_encryption(archive, 0, ZIP_EM_AES_256, password.c_str());
+
+    zip_close(archive);
+}
+int Mirage::Concatenate()
+{
+}
+Mirage::~Mirage()
+{
+}
---
+++ mirage.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include "zip.h"
+
+class Mirage
+{
+private:
+    std::string filePath,
+        password;
+
+public:
+    Mirage();
+    int ZipFile();
+    ~Mirage();
+};
